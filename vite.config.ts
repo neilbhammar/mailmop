ee75de333d26f0bc67392f88fe716ec6b9a79a5d
Neil Bhammar <nbhammar2000@gmail.com>
2025-03-12 01:23:45 -0400

    v14 Config Fixes for Deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,24 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'classic' }]
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+  build: {
+    target: 'es2015',
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
