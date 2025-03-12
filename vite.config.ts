5062da7210a6c7adcc722a0bc64464904875da10
Neil Bhammar <nbhammar2000@gmail.com>
2025-03-12 00:44:49 -0400

    Config fixes for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'automatic',
+      jsxImportSource: 'react',
+      babel: {
+        plugins: [
+          ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }]
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'react/jsx-runtime': 'react/jsx-runtime.js'
     },
   },
+  optimizeDeps: {
+    include: ['react/jsx-runtime']
+  }
 })
